2cf4e91e80dfb13c573147a8b33a41ae4ca3802b
coderspike <1922923386@qq.com>
2018-05-14 17:15:30 +0800

    123

--- src/main/java/com/example/demo/HelloSpringBootController.java
+++ src/main/java/com/example/demo/HelloSpringBootController.java
@@ -1,13 +1,67 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
 
 @RestController
 public class HelloSpringBootController {
     @RequestMapping("/sayHello")
     public String sayHello() {
-        return "Hello,World!";
+        String result = null;
+        try {
+            result = this.poolTest();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /*
+    线程池测试
+     */
+    public String poolTest() throws InterruptedException {
+        String result = "";
+        List<Integer> list = Collections.synchronizedList(new ArrayList<>());
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        final CountDownLatch countDownLatch = new CountDownLatch(2);
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < list.size(); i++) {
+            final int count = i;
+            executorService.execute(() -> {
+                try {
+                    show(count);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println("finally");
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();//当变为0时会立即调用
+        System.out.println("done");
+        result = "done";
+        executorService.shutdownNow();
+        return result;
+    }
+
+    private void show(Integer count) throws InterruptedException {
+        Thread.sleep(1200);
+        System.out.println(count);
+        Thread.sleep(1200);
     }
 }
+
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,50 @@
 package com.example.demo;
 
+import org.assertj.core.util.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        ExecutorService es = Executors.newFixedThreadPool(2);
+        final LinkedBlockingDeque<String> deque = new LinkedBlockingDeque<String>();
+        for (int i = 1; i <= 10; i++) {
+            deque.add(i + "");
+        }
+        es.submit(new Runnable() {
+            @Override
+            public void run() {
+                while (!deque.isEmpty()) {
+                    System.out.println(deque.poll() + "-" + Thread.currentThread().getName());
+                }
+            }
+        });
+        es.submit(new Runnable() {
+            @Override
+            public void run() {
+                while (!deque.isEmpty()) {
+                    System.out.println(deque.poll() + "-" + Thread.currentThread().getName());
+                }
+            }
+        });
+        try {
+            Thread.sleep(10000l);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
